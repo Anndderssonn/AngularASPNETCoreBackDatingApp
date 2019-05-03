7bfccc5b5e3de01a89dff61cb4b66a72aa808a3c
Andersson <andersonj.urbinav@gmail.com>
2019-05-02 22:20:42 -0500

    Register and Login (Token)

---
+++ Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(14, MinimumLength = 6, ErrorMessage = "You must specify password between 6 and 14 characters")]
+        public string Password { get; set; }
+    }
+}
